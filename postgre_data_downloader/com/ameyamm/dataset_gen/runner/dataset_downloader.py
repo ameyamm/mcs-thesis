27a7887a59d92c594985df3a0f596788d0dea77c
ameyamm-vote4db <ameya.malondkar@gmail.com>
2015-06-22 19:05:32 -0600

    Threading Introduced

--- postgre_data_downloader/com/ameyamm/dataset_gen/runner/dataset_downloader.py
+++ postgre_data_downloader/com/ameyamm/dataset_gen/runner/dataset_downloader.py
@@ -4,12 +4,29 @@ Created on Jun 18, 2015
 @author: ameya
 '''
 
-
+import threading
+import logging
 
 import psycopg2.extras
 
 from com.ameyamm.dataset_gen.runner import queries
 from com.ameyamm.dataset_gen.model.contact import Contact
+
+logging.basicConfig(filename = "datadownload-upload.log",
+        level=logging.DEBUG,
+        format='%(threadName)-10s : %(message)s',
+        )
+
+class dbthread(threading.Thread):
+    def __init__(self, threadProvince):
+        threading.Thread.__init__(self)
+        self.name = 'Thread-{}'.format(threadProvince) 
+        self.threadProvince = threadProvince
+
+    def run(self):
+        logging.debug('Starting thread for {}'.format(self.name))
+        loadTarget(threadName = self.name, threadProvince = self.threadProvince)
+        
 
 def getDBConnection():
     try :
@@ -21,24 +38,16 @@ def getDBConnection():
         print("Unable to connect to database")
         return None
 
-def loadTarget():
+def loadTarget(threadProvince, threadName = None):
     t_contact_conn = getDBConnection()        
-    t_marks_conn = getDBConnection()     
-    t_case_file_conn = getDBConnection()   
-    t_etag_conn = getDBConnection()
-    t_tag_conn = getDBConnection()
-    t_contact_campaign_region_conn = getDBConnection()
+    rec_conn = getDBConnection()   
     t_analytics_contact_conn = getDBConnection()
 
     t_contact_cursor = t_contact_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    t_marks_cursor = t_marks_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    t_case_file_cursor = t_case_file_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    t_etag_cursor = t_etag_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    t_tag_cursor = t_tag_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    t_contact_campaign_region_cursor = t_contact_campaign_region_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    record_cursor = rec_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     t_analytics_contact_cursor = t_analytics_contact_conn.cursor()
    
-    t_contact_cursor.execute(queries.SELECT_T_CONTACT)
+    t_contact_cursor.execute(queries.SELECT_T_CONTACT.format(threadProvince))
     for contactRec in t_contact_cursor:
         contact = Contact(
                           contact_id = contactRec['contact_id'],
@@ -86,83 +95,93 @@ def loadTarget():
         else:
             contact.townhouse_or_apartment = Contact.CONTACT_TOWNHOUSE
         
-        t_case_file_cursor = t_case_file_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        t_case_file_cursor.execute(queries.SELECT_T_CASE_FILE_FILED.format(contact.contact_id))
-        caseFiledRow = t_case_file_cursor.fetchone()
+        record_cursor.execute(queries.SELECT_T_CASE_FILE_FILED.format(contact.contact_id))
+        caseFiledRow = record_cursor.fetchone()
         contact.cases_filed = caseFiledRow['cases_filed']
-        
-        t_case_file_cursor.execute(queries.SELECT_T_CASE_FILE_CLOSED.format(contact.contact_id))
-        caseClosedRow = t_case_file_cursor.fetchone()
+
+        record_cursor.execute(queries.SELECT_T_CASE_FILE_CLOSED.format(contact.contact_id))
+        caseClosedRow = record_cursor.fetchone()
+
         contact.cases_closed = caseClosedRow['cases_closed']
         
         # find all associated tags
-        t_tag_cursor.execute(queries.SELECT_T_TAG_QUERY.format(contact.contact_id))
-        for tagRow in t_tag_cursor:
+        record_cursor.execute(queries.SELECT_T_TAG_QUERY.format(contact.contact_id))
+        tagRows = record_cursor.fetchall()
+        for tagRow in tagRows:
             contact.addTags(tagRow['name'])
         
         # find the campaigns and regions associated to create duplicate contacts
-        t_contact_campaign_region_cursor.execute(
+        record_cursor.execute(
                             queries.SELECT_DISTINCT_CONTACT_CAMPAIGN_REGION.format(contact.contact_id, contact.contact_id))
-        
-        print(contact.first_name)
+
+        logging.debug(contact.contact_id)
         copyContact = None
-        for distinctContactRow in t_contact_campaign_region_cursor:
+        distinctContactRows = record_cursor.fetchall()
+        for distinctContactRow in distinctContactRows:
             copyContact = contact.copy()
             copyContact.campaign_id = distinctContactRow['campaign_id']
             copyContact.region_id = distinctContactRow['region_id']
             
             # get marks 
             if copyContact.campaign_id is not None and copyContact.region_id is not None :  
-                t_marks_cursor.execute(queries.SELECT_T_CONTACT_MARKS.format(
+                record_cursor.execute(queries.SELECT_T_CONTACT_MARKS.format(
                                                                          copyContact.contact_id, 
                                                                          copyContact.campaign_id,
                                                                          copyContact.region_id))
-                marksRow = t_marks_cursor.fetchone()
+                marksRow = record_cursor.fetchone()
                 if marksRow is not None : 
                     copyContact.mark = marksRow['mark']
                     copyContact.leaning = marksRow['leaning']
                 
-            t_etag_cursor.execute(queries.SELECT_T_ENUM_TAG.format(
+            record_cursor.execute(queries.SELECT_T_ENUM_TAG.format(
                                                                    copyContact.contact_id,
                                                                    ' = ' + str(copyContact.campaign_id) if copyContact.campaign_id is not None else 'is null',
                                                                    ' = ' + str(copyContact.region_id) if copyContact.region_id is not None else 'is null'))
             
-            for etagRow in t_etag_cursor:
+            etagRows = record_cursor.fetchall()
+            for etagRow in etagRows:
                 copyContact.addEtags(etagRow['etag_name'],etagRow['etag_enum'])
             
             insertQuery = copyContact.getInsertQueryString()
-            t_analytics_contact_cursor.execute(insertQuery[0],insertQuery[1])    
+
+            try : 
+                t_analytics_contact_cursor.execute(insertQuery[0],insertQuery[1])    
+            except:
+                logging.debug("Exception CopyContact : {} : {}".format(
+                                                    copyContact.contact_id,e))
+                pass
+
             #print("{}".format(copyContact.getInsertQueryString()))
                 
-            
-            
         if copyContact is None:
             insertQuery = contact.getInsertQueryString()
-            t_analytics_contact_cursor.execute(insertQuery[0],insertQuery[1])
+            try : 
+                t_analytics_contact_cursor.execute(insertQuery[0],insertQuery[1])    
+            except Exception, e:
+                logging.debug("Exception Contact: {} : {}".format(
+                                                    contact.contact_id,e))
+                pass
 
     t_analytics_contact_conn.commit()
     
+    record_cursor.close()
     t_contact_cursor.close()
-    t_marks_cursor.close()
-    t_case_file_cursor.close()
-    t_etag_cursor.close()
-    t_tag_cursor.close()
-    t_contact_campaign_region_cursor.close()
     t_analytics_contact_cursor.close()
     
     t_contact_conn.close()
-    t_marks_conn.close()
-    t_case_file_conn.close()
-    t_etag_conn.close()
-    t_tag_conn.close()
-    t_contact_campaign_region_conn.close()
+    rec_conn.close()
     t_analytics_contact_conn.close()
     
     return
     
 def main():
-    loadTarget()
-    
+    provinces = [ 'AB', 'BC', 'MB', 'NB', 'NL', 'NS', 'NT', 'NU', 'ON', 'PE',
+            'QC', 'SK', 'YT' ]
+
+    threads = [dbthread(province) for province in provinces]
+
+    for thread in threads:
+        thread.start()
 
     return
     
